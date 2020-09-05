aa2f337aa63bcec1e9292cd1a09cbd4ca549f5de
Rufino Hdez <54823043+Rufino-Hdez@users.noreply.github.com>
2020-09-05 14:51:04 -0500

    Update db.php

--- config/db.php
+++ config/db.php
@@ -3,11 +3,11 @@
     class Database{
 
         public static function connect() {
-            // Crear variable y pasar parametros del servidor
+            
             $db = new mysqli('localhost', 'root', 'toor', 'tienda_master');
-            //Crear consulta para ver los resultado en castellano
+            
             $db->query("SET NAMES 'utf8'");
-            //Retornar la conexion
+            
             return $db;
         }
     }
